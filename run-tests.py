a2a7815a063879be5b8fa915f8205450ab40174c
kcaliban <fabian971@web.de>
2021-11-22 17:12:13 +0100

    test for regalloc

--- run-tests.py
+++ run-tests.py
@@ -6,5 +6,9 @@ import compiler
 
 compiler = compiler.Compiler()
 
-if run_tests("var", compiler, "var", None, interp_Pvar.InterpPvar().interp_P, None, None):
+results = [run_tests("var", compiler, "var", None, interp_Pvar.InterpPvar().interp_P, None, None)]
+if hasattr(compiler, "allocate_registers"):
+    results.append(run_tests("regalloc", compiler, "regalloc", None, interp_Pvar.InterpPvar().interp_P, None, None))
+
+if all(results):
     print("ALL TESTS PASSED! :D")
