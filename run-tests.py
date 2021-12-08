78ceeebee0f803bc2784a16ebebe7f9721b164f9
kcaliban <fabian971@web.de>
2021-12-08 22:30:41 +0100

    fix

--- run-tests.py
+++ run-tests.py
@@ -12,9 +12,9 @@ compiler = compiler.Compiler()
 
 
 enable_tracing()
-results = [run_tests("var", compiler, "var", None, interp_Pvar.InterpPvar().interp_P, None, None)]
+results = [run_tests("var", compiler, "var", None, interp_Pvar.InterpPvar().interp_P, type_check_Cif.TypeCheckCif().type_check, interp_Cif.InterpCif().interp)]
 if hasattr(compiler, "allocate_registers"):
-    results.append(run_tests("regalloc", compiler, "regalloc", None, interp_Pvar.InterpPvar().interp_P, None, None))
+    results.append(run_tests("regalloc", compiler, "regalloc", None, interp_Pvar.InterpPvar().interp_P, type_check_Cif.TypeCheckCif().type_check, interp_Cif.InterpCif().interp))
 if hasattr(compiler, "shrink"):
     results.append(run_tests("lif", compiler, "lif", type_check_Lif.TypeCheckLif().type_check, interp_Lif.InterpLif().interp, type_check_Cif.TypeCheckCif().type_check, interp_Cif.InterpCif().interp))
 
